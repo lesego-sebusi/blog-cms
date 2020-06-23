32ae7ed803099eee389aad5ef1277b9f884d68e3
Leslo360 <leslo912sebusi@gmail.com>
2020-06-23 14:31:12 +0200

    fix google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-168803358-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
